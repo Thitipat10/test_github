63a9a370dc7cdf37176ee28bd096fec6d92be766
Thitipat10 <thitipat1010@gmail.com>
2022-01-18 20:16:21 +0700

    test

--- admin/index.php
+++ admin/index.php
@@ -5,11 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TestGithub</title>
 </head>
 
 <body>
-    <h1>test</h1>
+    <h1>test asdad</h1>
+    asd;jasdokjasd
+    as;dljkaspdlj
+    <div class="row">
+        <div class="col-8"></div>
+        <div class="col-4"></div>
+    </div>
 </body>
 
 </html>
